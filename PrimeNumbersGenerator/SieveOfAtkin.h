31b49bad9a0495a3a0e4e350addee956bc6a2848
Anthony Marchenko <anthonymarchenko@gmail.com>
2015-04-29 01:41:50 +0300

    implement SieveOfAtrkin

---
+++ PrimeNumbersGenerator/SieveOfAtkin.h
@@ -0,0 +1,15 @@
+//
+//  PrimeNumbersGenerator.h
+//  PrimeNumbersGenerator
+//
+//  Created by Anthony Marchenko on 4/29/15.
+//  Copyright (c) 2015 Anthony Marchenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SieveOfAtkin : NSObject
+
++ (NSArray *) generatePrimeNumberTill: (int) limit;
+
+@end
--- PrimeNumbersGenerator/ViewController.m
+++ PrimeNumbersGenerator/ViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "ViewController.h"
+#import "NMRangeSlider.h"
+#import "SieveOfAtkin.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet NMRangeSlider *labelSlider;
+
+@property (weak, nonatomic) IBOutlet UILabel *lowerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *upperLabel;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *atkingPerformanceLabel;
 
 @end
 
@@ -17,11 +28,72 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //self.labelSlider.stepValue = 1.0f;
+    
+    [self configureLabelSlider];
+    [self updateSliderLabels];
+}
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [self updateSliderLabels];
+    
+    if([self.view respondsToSelector:@selector(setTintColor:)])
+    {
+        self.view.tintColor = [UIColor blueColor];
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - IBActions
+
+- (IBAction)generateButtonPressed:(UIButton *)sender {
+    
+    self.textView.text = [[[SieveOfAtkin generatePrimeNumberTill:[self.upperLabel.text intValue]]  valueForKey:@"description"] componentsJoinedByString:@", "];
+}
+
+// Handle control value changed events just like a normal slider
+- (IBAction)labelSliderChanged:(NMRangeSlider*)sender
+{
+    [self updateSliderLabels];
+    
+}
+
+- (void) configureLabelSlider
+{
+    self.labelSlider.minimumValue = 0;
+    self.labelSlider.maximumValue = 214748;
+    
+    self.labelSlider.lowerValue = 0;
+    self.labelSlider.upperValue = 214748;
+    self.labelSlider.minimumRange = 10;
+}
+
+- (void) updateSliderLabels
+{
+    // You get get the center point of the slider handles and use this to arrange other subviews
+    
+    CGPoint lowerCenter;
+    //lowerCenter.x = (self.labelSlider.lowerCenter.x + self.labelSlider.frame.origin.x);
+    lowerCenter.x = (self.labelSlider.lowerCenter.x + self.labelSlider.frame.origin.x);
+    lowerCenter.y = (self.labelSlider.center.y - 30.0f);
+    self.lowerLabel.center = lowerCenter;
+    self.lowerLabel.text = [NSString stringWithFormat:@"%d", (int)self.labelSlider.lowerValue];
+    
+    CGPoint upperCenter;
+    upperCenter.x = (self.labelSlider.upperCenter.x + self.labelSlider.frame.origin.x);
+    upperCenter.y = (self.labelSlider.center.y - 30.0f);
+    self.upperLabel.center = upperCenter;
+    self.upperLabel.text = [NSString stringWithFormat:@"%d", (int)self.labelSlider.upperValue];
+}
+
 
 @end
